58bae554efd75cb1708ca10fe5aefe09810d4037
kexin <apple@Kexin.hub>
2023-10-20 20:54:46 +1030

    trial

--- api/index.js
+++ api/index.js
@@ -31,7 +31,7 @@ app.options("/", (req, res) => {
   });
 
   app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", true);
+    res.header("Access-Control-Allow-Origin", "true");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
