843402e0ab1adf6ef35d206592b160cfd2b92180
kexin <apple@Kexin.hub>
2023-10-20 20:04:33 +1030

    readded creadentials true

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 
 
 app.use(cors({
-    // credentials: true,
+    credentials: true,
     // origin:['http://localhost:5173', 'https://mini-airbnb-website.onrender.com', 'https://mini-airbnb-website-api.onrender.com'],
     // methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
     // allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
