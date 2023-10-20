cd86cdd5ebee98c82294039cb14057d4bfa608dd
kexin <apple@Kexin.hub>
2023-10-20 20:43:02 +1030

    trial

--- api/index.js
+++ api/index.js
@@ -30,12 +30,11 @@ app.options("/", (req, res) => {
     res.sendStatus(204);
   });
 
-app.use((req, res, next) => {
-res.setHeader("Access-Control-Allow-Origin", "https://mini-airbnb-website.onrender.com");
-res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-next();
-})
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://mini-airbnb-website.onrender.com");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 // app.use(cors({
 //     credentials: true,
